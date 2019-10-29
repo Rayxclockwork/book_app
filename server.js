9805b1adf3905979aa8df59dd0d4c60ad80da855
RAVEN DELANEY <ravend17@gmail.com>
2019-10-29 10:09:01 -0700

    deploy 1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/hello', newPage);
+app.get('/', newPage);
 
 function newPage(req, res){
   res.render('pages/index');
