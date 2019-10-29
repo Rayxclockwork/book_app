5bf4297b74154902dceaee529a168cbcb0e69923
RAVEN DELANEY <ravend17@gmail.com>
2019-10-29 09:58:28 -0700

    ready for heroku deploy 1

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.get('/hello', newPage);
+
+function newPage(req, res){
+  res.render('pages/index');
+}
+
+app.listen(PORT, () => {
+  console.log(`living on ${PORT}`);
+})
